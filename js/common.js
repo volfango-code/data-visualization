a334b77f605b50dbdc1c2faf8f231d53035bff10
volfango <1255212783@qq.com>
2023-05-02 09:53:28 +0800

    axios配置token请求头及响应拦截器

--- js/common.js
+++ js/common.js
@@ -6,5 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 200)
 })
 
-// 配置axios根路径
+// 配置axios
+// 根路径
 axios.defaults.baseURL = 'http://ajax-api.itheima.net'
+// 请求头携带token
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+
+  // 可能没有携带token或token已经过期
+  if (error.response.status === 401) {
+    // 跳转到登录页
+    location.href = './login.html'
+  }
+
+  return Promise.reject(error);
+});
